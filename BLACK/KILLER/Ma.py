0c1ae7dee1bf0ab63d7db0068b354197f2739698
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-11-02 08:35:16 +0530

    Update Ma.py

--- BLACK/KILLER/Ma.py
+++ BLACK/KILLER/Ma.py
@@ -1,6 +1,27 @@
+#CYBER NAME BLACK-KILLER
+#GITHUB: https://github.com/ShuBhamg0sain
+#WHATAPP NO +919557777030
+import os
+CorrectUsername = "g0sain"
+CorrectPassword = "sim"
+
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input("\033[1;96m[#] \x1b[0;36m Enter Username\x1b[1;92m➤ ")
+    if (username == CorrectUsername):
+    	password = raw_input("\033[1;96m[#] \x1b[0;36m Enter Password\x1b[1;92m➤ ")
+        if (password == CorrectPassword):
+            print "Logged in successfully as " + username #fb-cloning-id SG
+            loop = 'false'
+        else:
+            print "Wrong password!"
+            os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
+    else:
+        print "Wrong username!"
+        os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
 import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
 os.system('rm -rf .txt')
-for n in range(20000):
+for n in range(200000):
 
     nmbr = random.randint(1111111, 9999999)
 
@@ -49,23 +70,36 @@ def cb():
     os.system('clear')
 ##### LOGO #####
 logo='''
-\033[1;91m█▀▄▀█ █▀▀█ █▀▀ ░▀░ █▀▀█
-\033[1;93m█░▀░█ █▄▄█ █▀▀ ▀█▀ █▄▄█
-\033[1;92m▀░░░▀ ▀░░▀ ▀░░ ▀▀▀ ▀░░▀
-
-\033[1;97m█░█ ░▀░ █░░ █░░ █▀▀ █▀▀█
-\033[1;96m█▀▄ ▀█▀ █░░ █░░ █▀▀ █▄▄▀
-\033[1;95m▀░▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀░▀▀
-
-
-
-\033[0;95m╭════════════════════════════════════════════╮
-\033[0;91m║\033[0;91mNAME : \033[0;92mXP-FAROOQ ANSARI                     \033[0;91m      ║
-\033[0;91m║\033[0;91mWHATSAPP NO :\033[0;92m +92132197796                     \033[0;91m     ║
-\033[0;91m║\033[0;91mNOTE :\033[0;92m PLZ DON,T CALL ME ONLY TEXT   \033[0;91m   ║
-\033[0;95m╰════════════════════════════════════════════╯
-                                '''
-
+\033[1;96m•◈•───────────────•◈•\033[1;92mShuBhamg0sain\033[1;96m•◈•───────────────•◈•
+\033[1;97m	                                    
+\033[1;97m                      :::!~!!!!!:. 
+\033[1;97m                  .xUHWH!! !!?M88WHX:.
+\033[1;97m                .X*#M@$!!  !X!M$$$$$$WWx:.
+\033[1;97m               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+\033[1;97m              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+\033[1;97m             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+\033[1;91m             ~!~!!!! .: BLACK-KILLER$$$$RMM!
+\033[1;97m               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+\033[1;97m               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+\033[1;97m             :X- M$$$$       `"T#$T~!8$WUXU~
+\033[1;97m            :%`  ~#$$$m:        ~!~ ?$$$$$$
+\033[1;97m          :!`.-   ~T$$$$8xx.  .xWW- ~""##*" 
+\033[1;97m.....   -~~\033[1;91m:<` !    ~?T#$$@@W@*?$$      /`
+\033[1;97mW$@@M!!! .!~~ \033[1;91m!!     .:XUW$W!~ `"~:    :
+\033[1;97m#"~~`.:x%`!!  \033[1;91m!H:   !WM$$$$Ti.: .!WUn+!`
+\033[1;97m:::~:!!`:X~ .:\033[1;92m ?H.!u "$$$B$$$!W:U!T$$M~
+\033[1;97m.~~   :X@!.-~   \033[1;92m?@WTWo("*$$$W$TH$! `
+\033[1;97mWi.~!X$?!-~    : \033[1;92m?$$$B$Wu("**$RM!
+\033[1;97m$R@i.~~ !     :   \033[1;92m~$$$$$B$$en:``    
+\033[1;97m?MXT@Wx.~    :     \033[1;92m~"##*$$$$M~   
+\033[1;47m                  \033[1;31mShuBhamg0sain                \033[1;0m
+\x1b[1;93m--------------------------------------------------------------
+\x1b[1;92m➣  NAME  : Shubhamg0sain
+\x1b[1;91m➣  CYBER NAME : BLACK-KILLER
+\x1b[1;93m➣  WHATSAPP NO     : +919557777030
+\x1b[1;95m➣  WARNING  : DON,T CALL ME ONLY TEXT
+\x1b[1;97m➣  NOTE     : USE FAST 4G SIM NET
+\x1b[1;93m--------------------------------------------------------------
 back = 0
 successful = []
 cpb = []
@@ -271,7 +305,7 @@ def action():
                         raw_input("\n[ Back ]")
                         menu()
         elif bch =="13":
-                os.system('xdg-open https://m.youtube.com/channel/UCFrYWrvT_TobefkZDD3sa_w')
+                os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
                 login()
         elif bch =='0':
                 exb()
@@ -479,7 +513,7 @@ def action():
         print '[✓] Total OK/CP : '+str(len(oks))+'/'+str(len(cpb))
         print('[✓] CP File Has Been Saved : save/checkpoint.txt')
         raw_input('\n[Press Enter To Go Back]')
-        os.system('python2 .README.md')
+        os.system('python2 S.py')
 
 if __name__ == '__main__':
         menu()
